07a9769b0402a9b566993f99fa556a417ff6d093
Freddy Barrios <freddybarrios.077@gmail.com>
2022-06-27 13:15:19 -0500

    Add dotenv

--- nuapp-api/src/main.ts
+++ nuapp-api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+const { PORT } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
