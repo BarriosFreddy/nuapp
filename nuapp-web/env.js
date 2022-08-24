9709483d3899775af0e77ef32eba50bc5c61f577
Freddy Barrios <freddybarrios.077@gmail.com>
2022-08-23 23:38:52 -0500

    new commit

--- nuapp-web/env.js
+++ nuapp-web/env.js
@@ -1,6 +1,6 @@
 const env = {
-  //BASE_URL: "https://nuapp-dev.herokuapp.com",
-  BASE_URL: "http://localhost:3001",
+  BASE_URL: "https://nuapp-dev.herokuapp.com",
+  //BASE_URL: "http://localhost:3001",
 };
 
 export default env;
