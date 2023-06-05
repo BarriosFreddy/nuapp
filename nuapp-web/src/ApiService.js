96fd5183964777e949651036b1773abe9627458a
Freddy Barrios <freddybarrios.077@gmail.com>
2023-06-05 11:15:38 -0500

    Add retry logic

--- nuapp-web/src/ApiService.js
+++ nuapp-web/src/ApiService.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
 const { REACT_APP_BASE_URL } = process.env
+const MAX_RETRY_ATTEMPTS = 3
 
 const axiosInstance = axios.create({
   baseURL: REACT_APP_BASE_URL,
-  timeout: 5000,
+  timeout: 10000,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
@@ -14,25 +15,46 @@ const axiosInstance = axios.create({
 
 export const ApiService = {
   async post(uri, data) {
-    return axiosInstance({
-      url: uri,
-      method: 'POST',
-      data,
-    })
+    return retry(() =>
+      axiosInstance({
+        url: uri,
+        method: 'POST',
+        data,
+      }),
+    )
   },
   async put(uri, data) {
-    return await axiosInstance({
-      url: uri,
-      method: 'PUT',
-      withCredentials: true,
-      data,
-    })
+    return await retry(() =>
+      axiosInstance({
+        url: uri,
+        method: 'PUT',
+        withCredentials: true,
+        data,
+      }),
+    )
   },
   async get(uri) {
-    return await axiosInstance({
-      url: uri,
-      method: 'GET',
-      withCredentials: true,
-    })
+    return await retry(() =>
+      axiosInstance({
+        url: uri,
+        method: 'GET',
+        withCredentials: true,
+      }),
+    )
   },
+}
+
+async function retry(request, retryAttempts = 0) {
+  let response = null
+  try {
+    response = await request()
+    if (![200, 201].includes(response.status)) {
+      if (retryAttempts >= MAX_RETRY_ATTEMPTS) return response
+      return await retry(request, ++retryAttempts)
+    }
+    return response
+  } catch (error) {
+    if (retryAttempts >= MAX_RETRY_ATTEMPTS) return response
+    return await retry(request, ++retryAttempts)
+  }
 }
