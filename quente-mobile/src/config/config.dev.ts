c73aa776617eb77cf73946db30e9922ebab043ee
Freddy Barrios <freddybarrios.077@gmail.com>
2024-09-28 15:04:30 -0500

    ADD fastlane configuration

--- quente-mobile/src/config/config.dev.ts
+++ quente-mobile/src/config/config.dev.ts
@@ -6,6 +6,6 @@
  * https://reactnative.dev/docs/security#storing-sensitive-info
  */
 export default {
-  API_URL: "http://192.168.1.7:3001/api/v1/",
-//  API_URL: "http://192.168.45.97:3001/api/v1/", // mobile data
+  //API_URL: "http://192.168.1.7:3001/api/v1/",
+  API_URL: "http://192.168.29.97:3001/api/v1/", // mobile data
 }
