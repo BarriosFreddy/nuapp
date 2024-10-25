813e74122915249296cfbffd8aed30afd00f8692
Freddy Barrios <freddybarrios.077@gmail.com>
2024-10-25 07:59:44 -0500

    ADD local creation for items

--- quente-ui/common/modules/inventory/services/items.service.js
+++ quente-ui/common/modules/inventory/services/items.service.js
@@ -1,6 +1,6 @@
-import { quenteDB } from '../../../shared/db/indexDB'
+import { quenteDB } from "../../../shared/db/indexDB";
 import indexDBService from "../../../shared/services/indexDB.service";
-
+import { hexoid } from "hexoid";
 import {
   saveSuccess,
   setItems,
@@ -8,73 +8,79 @@ import {
   setExistsByCode,
   setSaving,
   setFetching,
-} from '../reducers/items.reducer'
-import isOnline from 'is-online'
-let isonline = false
+} from "../reducers/items.reducer";
+import isOnline from "is-online";
+let isonline = false;
+const OBJECT_ID_LENGTH = 24;
 
 export const saveItem = (item) => async (dispatch, _, api) => {
-  dispatch(setSaving(true))
-  const { status } = await api.post('/items', item)
-  dispatch(saveSuccess(status === 201))
-  dispatch(setSaving(false))
-}
+  dispatch(setSaving(true));
+  const _id = hexoid(OBJECT_ID_LENGTH)();
+  item._id = _id;
+  //const { status } = await api.post('/items', item)
+  const id = await quenteDB.items.add(item);
+  console.log({ id });
+  dispatch(saveSuccess(!!id));
+  dispatch(setSaving(false));
+};
 
 export const updateItem = (item) => async (dispatch, _, api) => {
-  dispatch(setSaving(true))
-  const itemToUpdate = { ...item }
-  const id = itemToUpdate._id
-  delete itemToUpdate._id
+  dispatch(setSaving(true));
+  const itemToUpdate = { ...item };
+  const id = itemToUpdate._id;
+  delete itemToUpdate._id;
   //await api.put(`/items/${id}`, itemToUpdate)
-  const wasUpdated = await quenteDB.items.update(id, itemToUpdate)
-  dispatch(saveSuccess(wasUpdated))
-  dispatch(setSaving(false))
-}
+  const wasUpdated = await quenteDB.items.update(id, itemToUpdate);
+  dispatch(saveSuccess(wasUpdated));
+  dispatch(setSaving(false));
+};
 
 export const saveItemCategoriesBulk = (items) => async (dispatch, _, api) => {
-  const { status } = await api.post('/items/bulk', items)
-  dispatch(saveSuccess(status === 201))
-}
+  const { status } = await api.post("/items/bulk", items);
+  dispatch(saveSuccess(status === 201));
+};
 
 export const existByCode = (code) => async (dispatch, _, api) => {
-  if (!code) return
-  const { data, status } = await api.get(`/items/code/${code}`)
-  if (status === 200) dispatch(setExistsByCode(data))
-}
+  if (!code) return;
+  //const { data, status } = await api.get(`/items/code/${code}`)
+  const isRegistered = await quenteDB.items.where("code").equals(code).first();
+  dispatch(setExistsByCode(!!isRegistered));
+};
 
 export const getItems =
   (queryParams, useCacheOnly = false) =>
   async (dispatch, getState, api) => {
-    dispatch(setFetching(true))
-/*     const urlQueryParams = new URLSearchParams(queryParams).toString()
+    dispatch(setFetching(true));
+    /*     const urlQueryParams = new URLSearchParams(queryParams).toString()
     isonline = useCacheOnly ? false : await isOnline()
     const { data, status } = isonline
       ? await api.get(`/items${urlQueryParams.length > 0 ? '?' + urlQueryParams.toString() : ''}`)
       : getLocally(getState(), queryParams) */
-      const status = 200
-      const data = await quenteDB.items
-        .where("name")
-        .startsWithIgnoreCase(queryParams.name || '')
-        .or('code')
-        .equals(queryParams.code || '')
-        .offset(((queryParams.page || 1)-1) * 10)
-        .limit(10)
-        .toArray();
-    if (status === 200) dispatch(setItems(data))
-    dispatch(setFetching(false))
-  }
+    const status = 200;
+    const data = await quenteDB.items
+      .where("name")
+      .startsWithIgnoreCase(queryParams.name || "")
+      .or("code")
+      .equals(queryParams.code || "")
+      .offset(((queryParams.page || 1) - 1) * 10)
+      .limit(10)
+      .toArray();
+    dispatch(setItems(data));
+    dispatch(setFetching(false));
+  };
 
 export const getAllItems = () => async (dispatch, state, api) => {
-  const { data, status } = await api.get(`/items?size=1000`)
-  if (status === 200) await indexDBService.bulkPutItems(data)
-}
+  const { data, status } = await api.get(`/items?size=1000`);
+  if (status === 200) await indexDBService.bulkPutItems(data);
+};
 
 function getLocally(state, queryParams) {
-  const { items } = state.items.offline
+  const { items } = state.items.offline;
   let data = items.filter(
     ({ code, name }) =>
       code.toUpperCase().includes(queryParams.code.toUpperCase()) ||
-      name.toUpperCase().includes(queryParams.name.toUpperCase()),
-  )
-  if (data.length > 10) data.length = 10
-  return { data, status: 200 }
+      name.toUpperCase().includes(queryParams.name.toUpperCase())
+  );
+  if (data.length > 10) data.length = 10;
+  return { data, status: 200 };
 }
--- quente-ui/common/modules/inventory/views/items/Items.js
+++ quente-ui/common/modules/inventory/views/items/Items.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from "react";
 
 import {
   CCard,
@@ -12,134 +12,147 @@ import {
   CFormInput,
   CFormSelect,
   CFormLabel,
-} from '@coreui/react'
-import ItemForm from './ItemForm'
-import { useDispatch, useSelector } from 'react-redux'
-import { getItems, getAllItems, saveItem, updateItem } from './../../services/items.service'
-import CONSTANTS from '../../../../constants'
-import { sendToast } from '../../../../shared/services/notification.service'
-import { Helmet } from 'react-helmet'
-import { useDidUpdateControl } from '../../../../hooks/useDidUpdateControl'
-import ItemList from './ItemList'
-import CIcon from '@coreui/icons-react'
-import { cilPlus, cilSearch, cilTrash } from '@coreui/icons'
+} from "@coreui/react";
+import ItemForm from "./ItemForm";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getItems,
+  getAllItems,
+  saveItem,
+  updateItem,
+} from "./../../services/items.service";
+import CONSTANTS from "../../../../constants";
+import { sendToast } from "../../../../shared/services/notification.service";
+import { Helmet } from "react-helmet";
+import { useDidUpdateControl } from "../../../../hooks/useDidUpdateControl";
+import ItemList from "./ItemList";
+import CIcon from "@coreui/icons-react";
+import { cilPlus, cilSearch, cilTrash } from "@coreui/icons";
 
-const { ENTER_KEYCODE, TAB_KEYCODE } = CONSTANTS
+
+const { ENTER_KEYCODE, TAB_KEYCODE } = CONSTANTS;
 
 const queryParamsInitial = {
   page: 1,
-}
+};
 
 function Item() {
-  const dispatch = useDispatch()
-  const items = useSelector((state) => state.items.items)
-  const saveSuccess = useSelector((state) => state.items.saveSuccess)
-  const saving = useSelector((state) => state.items.saving)
-  const fetching = useSelector((state) => state.items.fetching)
-  const [searchTerm, setSearchTerm] = useState('')
-  let [editing, setEditing] = useState(false)
-  let [copying, setCopying] = useState(false)
-  let [item, setItem] = useState(null)
-  const [showFilterSection, setShowFilterSection] = useState(false)
-  const [queryParams, setQueryParams] = useState(queryParamsInitial)
-  const searchInputRef = useRef()
+  const dispatch = useDispatch();
+  const items = useSelector((state) => state.items.items);
+  const saveSuccess = useSelector((state) => state.items.saveSuccess);
+  const saving = useSelector((state) => state.items.saving);
+  const fetching = useSelector((state) => state.items.fetching);
+  const [searchTerm, setSearchTerm] = useState("");
+  let [editing, setEditing] = useState(false);
+  let [copying, setCopying] = useState(false);
+  let [item, setItem] = useState(null);
+  const [showFilterSection, setShowFilterSection] = useState(false);
+  const [queryParams, setQueryParams] = useState(queryParamsInitial);
+  const searchInputRef = useRef();
 
   useEffect(() => {
-    setSearchTerm('')
-    dispatch(getItems({ page: 1 }))
-  }, [dispatch])
+    setSearchTerm("");
+    dispatch(getItems({ page: 1 }));
+  }, [dispatch]);
 
   useDidUpdateControl(
     () => {
       if (saveSuccess) {
-        setEditing(false)
-        sendToast(dispatch, { message: 'Guardado exitosamente!' })
-        setItem(null)
-        handleSearch({ page: queryParams.page })
+        setEditing(false);
+        sendToast(dispatch, { message: "Guardado exitosamente!" });
+        setItem(null);
+        handleSearch({ page: queryParams.page });
       } else {
-        sendToast(dispatch, { message: 'No se pudo guardar los datos', color: 'danger' })
+        sendToast(dispatch, {
+          message: "No se pudo guardar los datos",
+          color: "danger",
+        });
       }
     },
     saving,
-    [saveSuccess],
-  )
+    [saveSuccess]
+  );
 
   const handleSave = (item) => {
-    delete item.price // delete after making sure the changes
-    delete item.measurementUnit
-    delete item.cost
-    if (item._id) dispatch(updateItem(item))
-    else dispatch(saveItem(item))
-  }
+    delete item.price; // delete after making sure the changes
+    delete item.measurementUnit;
+    delete item.cost;
+    if (item._id) dispatch(updateItem(item));
+    else dispatch(saveItem(item));
+  };
 
   const handleCancel = async () => {
-    setEditing(false)
-  }
+    setEditing(false);
+  };
 
   const handlePrevPage = async () => {
-    const newParams = { ...queryParams, page: queryParams.page === 1 ? 1 : queryParams.page - 1 }
-    search(newParams)
-  }
+    const newParams = {
+      ...queryParams,
+      page: queryParams.page === 1 ? 1 : queryParams.page - 1,
+    };
+    search(newParams);
+  };
 
   const handleNextPage = async () => {
-    const newParams = { ...queryParams, page: queryParams.page + 1 }
-    search(newParams)
-  }
+    const newParams = { ...queryParams, page: queryParams.page + 1 };
+    search(newParams);
+  };
 
-  const onChangeField = ({ target: { value } }) => setSearchTerm(value)
+  const onChangeField = ({ target: { value } }) => setSearchTerm(value);
 
   const onKeyDownCodeField = async ({ keyCode }) => {
-    if ([ENTER_KEYCODE, TAB_KEYCODE].includes(keyCode)) handleSearch({ page: 1 })
-  }
+    if ([ENTER_KEYCODE, TAB_KEYCODE].includes(keyCode))
+      handleSearch({ page: 1 });
+  };
 
   const search = async (params = {}) => {
-    setQueryParams(params)
+    setQueryParams(params);
     //dispatch(getAllItems());
-    dispatch(getItems(params))
-  }
+    dispatch(getItems(params));
+  };
 
   const handleSearch = (params = {}) => {
-    const newParams = { ...params }
+    const newParams = { ...params };
     if (!!searchTerm) {
-      newParams.code = searchTerm.trim()
-      newParams.name = searchTerm.trim()
+      newParams.code = searchTerm.trim();
+      newParams.name = searchTerm.trim();
     }
-    search(newParams)
-  }
+    search(newParams);
+  };
 
   const handleEdit = (item) => {
-    setEditing(true)
-    setCopying(false)
-    setItem(item)
-  }
+    setEditing(true);
+    setCopying(false);
+    setItem(item);
+  };
 
   const handleCopy = (item) => {
-    setCopying(true)
-    setEditing(true)
-    setItem(item)
-  }
+    setCopying(true);
+    setEditing(true);
+    setItem(item);
+  };
 
   const handleNewItem = () => {
-    setItem(null)
-    setEditing(true)
-    setCopying(false)
-  }
+    setItem(null);
+    setEditing(true);
+    setCopying(false);
+  };
 
   const handleClear = () => {
-    setSearchTerm('')
-    const params = { page: 1 }
-    search(params)
-    searchInputRef.current.focus()
-  }
+    setSearchTerm("");
+    const params = { page: 1 };
+    search(params);
+    searchInputRef.current.focus();
+  };
 
   const handleFilter = () => {
-    setShowFilterSection(!showFilterSection)
-  }
+    setShowFilterSection(!showFilterSection);
+  };
 
   const handleChangeFilters = ({ target: { name, value } }) => {
-    const params = { ...queryParams, page: 1, [name]: value }
-    search(params)
-  }
+    const params = { ...queryParams, page: 1, [name]: value };
+    search(params);
+  };
 
   return (
     <>
@@ -155,7 +168,11 @@ function Item() {
                   <CRow>
                     <CCol xs="2" lg="3">
                       ITEMS &nbsp;
-                      <CButton variant="outline" color="success" onClick={handleNewItem}>
+                      <CButton
+                        variant="outline"
+                        color="success"
+                        onClick={handleNewItem}
+                      >
                         <div className="d-none d-lg-block">NUEVO</div>
                         <div className="d-lg-none">
                           <CIcon icon={cilPlus} size="sm" />
@@ -205,7 +222,10 @@ function Item() {
                     {showFilterSection && (
                       <>
                         <CCol xs="2" lg="1" className="text-end">
-                          <CFormLabel htmlFor="stock" className="col-form-label">
+                          <CFormLabel
+                            htmlFor="stock"
+                            className="col-form-label"
+                          >
                             Stock
                           </CFormLabel>
                         </CCol>
@@ -217,10 +237,10 @@ function Item() {
                             name="stock"
                             size="sm"
                             options={[
-                              { label: 'TODOS', value: '' },
-                              { label: 'DISPONIBLE', value: 'IS' },
-                              { label: 'RECOMPRA', value: 'RP' },
-                              { label: 'NO DISPONIBLE', value: 'WS' },
+                              { label: "TODOS", value: "" },
+                              { label: "DISPONIBLE", value: "IS" },
+                              { label: "RECOMPRA", value: "RP" },
+                              { label: "NO DISPONIBLE", value: "WS" },
                             ]}
                             onChange={handleChangeFilters}
                           />
@@ -232,7 +252,7 @@ function Item() {
               )}
               {editing && (
                 <div className="d-none d-lg-block">
-                  {item ? (copying ? 'COPIANDO' : 'EDITANDO') : 'CREANDO'} ITEM
+                  {item ? (copying ? "COPIANDO" : "EDITANDO") : "CREANDO"} ITEM
                 </div>
               )}
             </CCardHeader>
@@ -250,13 +270,18 @@ function Item() {
               />
             )}
             {editing && (
-              <ItemForm copying={copying} item={item} onSave={handleSave} onCancel={handleCancel} />
+              <ItemForm
+                copying={copying}
+                item={item}
+                onSave={handleSave}
+                onCancel={handleCancel}
+              />
             )}
           </CCardBody>
         </CCard>
       </CContainer>
     </>
-  )
+  );
 }
 
-export default Item
+export default Item;
