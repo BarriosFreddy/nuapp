0d99dbf08a087acb44d416eb04da8603f97f3daf
Freddy Barrios <freddybarrios.077@gmail.com>
2025-01-16 22:19:52 -0500

    Delete old repo

--- quente-mobile/src/types.d/react-native-config.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'react-native-config' {
-    export interface NativeConfig {
-        WOMPI_API_URL?: string;
-        WOMPI_API_PUB_KEY?: string;
-    }
-    
-    export const Config: NativeConfig
-    export default Config
-  }
